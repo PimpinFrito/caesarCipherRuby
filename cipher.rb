c3aac31cdbe3305659bff2fbee235674996f4776
Fritz T <fritzt14@icloud.com>
2022-09-19 20:22:54 -0400

    Started cipher method

---
+++ cipher.rb
@@ -0,0 +1,13 @@
+def caesarCipher(string, cipher)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  stringList = string.split('')
+  puts string
+  puts stringList
+  cipherList = stringList.map do |letter|
+    letter.
+    
+  end
+end
+
+
+caesarCipher("ap le", 3)
