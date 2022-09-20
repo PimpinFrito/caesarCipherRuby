7cc46d77bb5fc4a16418771e8491893029675e4f
Fritz T <fritzt14@icloud.com>
2022-09-19 21:36:11 -0400

    Does not preserve spaces currently

--- cipher.rb
+++ cipher.rb
@@ -1,13 +1,27 @@
-def caesarCipher(string, cipher)
+def caesarCipher(string, shiftFactor)
   alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
   stringList = string.split('')
-  puts string
-  puts stringList
   cipherList = stringList.map do |letter|
-    letter.
-    
+    p letter
+    cipher_letter = getCipher(letter, shiftFactor) if alphabet.include?(letter)
+    cipher_letter
   end
+  cipherList.f
 end
 
+def getCipher(letter, shiftFactor)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  index = alphabet.index(letter)
+  # puts "index = #{index}"
+  newIndex = index + shiftFactor
+  # puts "New Index = #{newIndex}"
+  newIndex -= alphabet.length while newIndex > alphabet.length
+  alphabet[newIndex]
+end
 
-caesarCipher("ap le", 3)
+caesarCipher('ap le', 3)
+
+puts 'Test 1: apple shifted 3 -> dssoh'
+puts "#{caesarCipher('apple', 3)} == 'dssoh':#{caesarCipher('apple', 3) == 'dssoh'}"
+puts 'Test 1: target sauce shifted 3 -> 	wdujhw vdxfh'
+puts "#{caesarCipher('target sauce', 3)} == 'wdujhw vdxfh'': #{caesarCipher('target sauce', 3) == 'wdujhw vdxfh'}"
