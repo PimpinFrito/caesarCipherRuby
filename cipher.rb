7fade0b47c3e1eb50c578861f335f1df2481dad5
Fritz T <fritzt14@icloud.com>
2022-09-19 22:19:20 -0400

    Basically Done, just needs code cleanup

--- cipher.rb
+++ cipher.rb
@@ -1,27 +1,35 @@
 def caesarCipher(string, shiftFactor)
   alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-  stringList = string.split('')
-  cipherList = stringList.map do |letter|
-    p letter
-    cipher_letter = getCipher(letter, shiftFactor) if alphabet.include?(letter)
-    cipher_letter
+  cipherString = ''
+  string.each_char do |char|
+    lowerChar = char.downcase
+    cipherChar = if alphabet.include?(lowerChar)
+                   getCipher(lowerChar, shiftFactor, alphabet)
+                 else
+                   char
+                 end
+
+    cipherChar = cipherChar.upcase if char == char.upcase
+    cipherString += cipherChar
   end
-  cipherList.f
+  cipherString
 end
 
-def getCipher(letter, shiftFactor)
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+def getCipher(letter, shiftFactor, alphabet)
   index = alphabet.index(letter)
-  # puts "index = #{index}"
   newIndex = index + shiftFactor
-  # puts "New Index = #{newIndex}"
   newIndex -= alphabet.length while newIndex > alphabet.length
   alphabet[newIndex]
 end
-
-caesarCipher('ap le', 3)
 
 puts 'Test 1: apple shifted 3 -> dssoh'
 puts "#{caesarCipher('apple', 3)} == 'dssoh':#{caesarCipher('apple', 3) == 'dssoh'}"
 puts 'Test 1: target sauce shifted 3 -> 	wdujhw vdxfh'
 puts "#{caesarCipher('target sauce', 3)} == 'wdujhw vdxfh'': #{caesarCipher('target sauce', 3) == 'wdujhw vdxfh'}"
+'Test 1: target sauce shifted 30 -> 	wdujhw vdxfh'
+puts "#{caesarCipher('target sauce', 30)} == 'xevkix weygi': #{caesarCipher('target sauce', 30) == 'xevkix weygi'}"
+'Test 1: RENO 911 shifted 15 -> GTCD 911'
+
+puts ' '
+
+puts "#{caesarCipher('RENO 911', 15)} == 'GTCD 911': #{caesarCipher('RENO 911', 15) == 'GTCD 911'}"
